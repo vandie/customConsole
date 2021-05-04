640599cc6b052b5d846ad076555be9ea3039f19c
vandie <mikevdv@vandie.co.uk>
2021-05-04 07:21:00 +0100

    feat: fix published version

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,15 @@
 import typescript from '@rollup/plugin-typescript';
 import * as pkg from './package.json';
 
-export default {
+export default [{
   input: 'src/index.ts',
   output: [
     {
       file: pkg.main,
       format: 'cjs',
+      exports: 'named',
       sourcemap: true,
     }
   ],
   plugins: [typescript()]
-};
+}];
