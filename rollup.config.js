1d8334c00365e98a996443f0110ba6a804ebce12
vandie <mikevdv@vandie.co.uk>
2021-05-04 07:37:14 +0100

    release: release 1.0.4

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 import * as pkg from './package.json';
 
 export default [{
@@ -9,7 +9,20 @@ export default [{
       format: 'cjs',
       exports: 'named',
       sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true
     }
   ],
-  plugins: [typescript()]
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+  plugins: [
+    typescript({
+      typescript: require('typescript'),
+    })
+  ]
 }];
